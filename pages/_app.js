25925e2f5a25400d842035cd60ecabcafa0fd2c0
dschmidt1910-svg <d.schmidt1910@gmail.com>
2026-03-01 21:48:37 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,5 @@
-import { useEffect } from 'react'
-import { supabase } from '../lib/supabaseClient'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
-
-  useEffect(() => {
-    // nur auf Client ausführen
-    if (typeof window !== 'undefined') {
-      supabase.auth.getSessionFromUrl({ storeSession: true })
-        .then(({ data, error }) => {
-          if(error) console.log("Fehler beim Magic Link:", error.message)
-          else if(data.session) console.log("User eingeloggt:", data.session.user.email)
-        })
-    }
-  }, [])
-
   return <Component {...pageProps} />
 }

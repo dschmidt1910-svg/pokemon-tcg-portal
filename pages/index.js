dd7274ae00b3da58fc3fd74df16576bc3563565d
dschmidt1910-svg <d.schmidt1910@gmail.com>
2026-03-01 20:56:17 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import { supabase } from '../lib/supabaseClient'
 
 export default function Home() {
+  const cards = [
+    { id: 1, name: "Pikachu", set: "Base Set", image: "https://images.pokemontcg.io/base1/58.png" },
+    { id: 2, name: "Glurak", set: "Base Set", image: "https://images.pokemontcg.io/base1/4.png" },
+    { id: 3, name: "Bisasam", set: "Base Set", image: "https://images.pokemontcg.io/base1/1.png" }
+  ];
+
   return (
     <div style={{padding: 40}}>
-      <h1>Supabase Client funktioniert ✅</h1>
-    </div>
-  )
-}
+      <h1>Deutsche Pokémon Testkarten</h1>

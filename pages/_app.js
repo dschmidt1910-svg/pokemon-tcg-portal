0e97ca2c160875cfcbe334946295470508104d6c
dschmidt1910-svg <d.schmidt1910@gmail.com>
2026-03-01 21:46:43 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,14 @@ import '../styles/globals.css'
 export default function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
-    // Prüfen, ob URL Magic Link Parameter enthält
-    const handleAuth = async () => {
-      const { data, error } = await supabase.auth.getSessionFromUrl({ storeSession: true })
-      if(error) console.log("Fehler beim Magic Link:", error.message)
-      else if(data.session) console.log("User eingeloggt:", data.session.user.email)
+    // nur auf Client ausführen
+    if (typeof window !== 'undefined') {
+      supabase.auth.getSessionFromUrl({ storeSession: true })
+        .then(({ data, error }) => {
+          if(error) console.log("Fehler beim Magic Link:", error.message)
+          else if(data.session) console.log("User eingeloggt:", data.session.user.email)
+        })
     }
-
-    handleAuth()
   }, [])
 
   return <Component {...pageProps} />
